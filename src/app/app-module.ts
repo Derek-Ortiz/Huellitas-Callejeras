6e3c3c26245f75014f8d7c9794e857cbfdecdaed
Derek-Ortiz <243742@ids.upchiapas.edu.mx>
2025-10-25 20:32:18 -0600

    creacion de proyecto y barra de navegacion

---
+++ src/app/app-module.ts
@@ -0,0 +1,23 @@
+import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Navegation } from './navegation/navegation';
+
+@NgModule({
+  declarations: [
+    App,
+    Navegation
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
