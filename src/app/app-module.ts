4d9538183e43051b9f0706403efcae0ab0fae989
Derek-Ortiz <243742@ids.upchiapas.edu.mx>
2025-11-02 21:20:28 -0600

    Creacion del componente añadir tratamiento

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -4,18 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { Navegation } from './navegation/navegation';
-import { Medicine } from './medicine/medicine';
-import { Tratamiento } from './tratamiento/tratamiento';
-import { ModalMedicine } from './modal-medicine/modal-medicine';
-import { RouterLink } from '@angular/router';
+
 
 @NgModule({
   declarations: [
     App,
     Navegation,
-    Medicine,
-    Tratamiento,
-    ModalMedicine,
   ],
   imports: [
     BrowserModule,
